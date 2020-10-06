c7d2f2b61712be19a8bfe7122b9aa3d487f3e7bc
Haidara Bassidiki <hbassidiki@gmail.com>
2020-10-06 11:38:23 +0200

    first commit

---
+++ RandomPwd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int int main(int argc, char const *argv[])
+{
+	int tab[23] = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}
+	for(int i = 0; i<23; i++){
+		printf("%d\n",tab[i]);
+	}
+	return 0;
+}
